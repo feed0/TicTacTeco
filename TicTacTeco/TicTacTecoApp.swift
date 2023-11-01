31a131b33cdc175153681fd5bf9aa7257427b52f
Felipe Campelo <iifcamp@icloud.com>
2023-11-01 14:21:49 -0300

    Initial Commit

---
+++ TicTacTeco/TicTacTecoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacTecoApp.swift
+//  TicTacTeco
+//
+//  Created by Felipe Campelo on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacTecoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
